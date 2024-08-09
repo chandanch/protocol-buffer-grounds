62f5ddb4fdef0639f20b476dfe0fc8d18556c6f2
chandanch <chandan.cec107@gmail.com>
2024-08-09 15:17:45 +0530

    serialize & deserialize message

--- app-js/app.js
+++ app-js/app.js
@@ -1,37 +1,52 @@
-const protobuf = require("protobufjs");
+const protobuf = require('protobufjs');
 
 // 1. load the message defn file
-protobuf.load("orderservice.proto").then((root) => {
-  // root object contains all the message types and other elements
-  // defined in the defn file
-  const Order = root.lookupType("roomservice.Order");
-  const Size = root.lookupEnum("roomservice.Size");
-  const Timestamp = root.lookupType("google.protobuf.Timestamp");
+protobuf.load('../order-service/orderservice.proto').then((root) => {
+	// root object contains all the message types and other elements
+	// defined in the defn file
+	const Order = root.lookupType('orderservice.Order');
+	const Size = root.lookupEnum('orderservice.Size');
+	const Timestamp = root.lookupType('google.protobuf.Timestamp');
 
-  // create payload - this is where we create the message
-  const payload = {
-    order_id: 12322,
-    order_time: Timestamp.create({ seconds: new Date().getTime() / 1000 }),
-    cost: 23.32,
-    meal: {
-      "chandio": {
-        entree: {
-          food: 'Burger',
-          notes: ['extra cheese'],
-          size: ''
-        },
-        drink: {
-          name: 'cola',
-          size: ''
-        },
-        sides: [
-          {
-            type: 'fries',
-            notes: ["extra zest"],
-            size: ''
-          }
-        ]
-      }
-    }
-  };
+	// create payload - this is where we create the message data
+	const payload = {
+		order_id: 12322,
+		order_time: Timestamp.create({ seconds: new Date().getTime() / 1000 }),
+		cost: 23.32,
+		meal: {
+			chandio: {
+				entree: {
+					food: 'Burger',
+					notes: ['extra cheese'],
+					size: Size.values.Size_MEDIUM,
+				},
+				drink: {
+					name: 'cola',
+					size: Size.values.Size_MEDIUM,
+				},
+				sides: [
+					{
+						type: 'fries',
+						notes: ['extra zest'],
+						size: Size.values.Size_MEDIUM,
+					},
+				],
+			},
+		},
+	};
+
+	// create the Order Protobuf message
+	// serializes the message (coverts it to binary)
+	const message = Order.create(payload);
+
+	// encode the message and wait till it finishes
+	const encodedMessage = Order.encode(message).finish();
+
+	console.log('Serialized Message:');
+	console.log(Buffer.from(encodedMessage).toString('ascii'));
+
+	// de-serialize the message - reconstitute into an object
+	console.log('De-Serialized Message:');
+	const order2 = Order.decode(encodedMessage);
+	console.log(order2.toJSON(order2));
 });
